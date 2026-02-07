40ba5963aee55c62245dbc4b10fe23610d7e1acb
masaki0710 <masaki0710.management@gmail.com>
2026-02-07 16:42:31 +0900

    feat: ゲームオーバースクリプトを作成

---
+++ Assets/Scripts/GameOverDetector.cs
@@ -0,0 +1,36 @@
+// ゲームオーバーを検出するスクリプト
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameOverDetector : MonoBehaviour
+{
+    private bool isGameOver = false;
+
+    void Update()
+    {
+        if (isGameOver) return;
+
+        // ゲームオーバー判定
+        if (transform.position.y < GameConfig.DeathYThreshold || transform.position.x < GameConfig.DeathXThreshold)
+        {
+            StartCoroutine(GameOverSequence());
+        }
+    }
+
+    IEnumerator GameOverSequence()
+    {
+        isGameOver = true;
+        Debug.Log("Game Over!");
+
+        Time.timeScale = GameConfig.SlowMotionScale;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+
+        yield return new WaitForSecondsRealtime(GameConfig.GameOverDelay);
+
+        Time.timeScale = 1.0f;
+        Time.fixedDeltaTime = 0.02f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
